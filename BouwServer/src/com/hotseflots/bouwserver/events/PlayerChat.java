97d04a8864655caaef56a4f84e19438720438631
Demian Brunt <42469956+DikkeDrieWieler@users.noreply.github.com>
2019-01-16 14:01:06 +0100

    Added: Mute module

--- BouwServer/src/com/hotseflots/bouwserver/events/PlayerChat.java
+++ BouwServer/src/com/hotseflots/bouwserver/events/PlayerChat.java
@@ -1,22 +1,43 @@
 package com.hotseflots.bouwserver.events;
 
+import com.hotseflots.bouwserver.Main;
 import com.hotseflots.bouwserver.UserManagerSystem;
+import com.hotseflots.bouwserver.utils.ConfigPaths;
 import com.hotseflots.bouwserver.utils.Messages;
 import org.bukkit.Bukkit;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.AsyncPlayerChatEvent;
 
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 public class PlayerChat implements Listener {
 
     @EventHandler
     public void onPlayerChat(AsyncPlayerChatEvent event) {
+
+        String expireDateString = Main.plugin.config.getString(ConfigPaths.GetDetailPath(event.getPlayer().getUniqueId().toString(), "mutes", Integer.parseInt(Main.plugin.config.getString(ConfigPaths.amountPath(event.getPlayer().getUniqueId().toString(), "mutes"))), "expires"));
+        DateFormat dateFormat = new SimpleDateFormat("dd/MM/yyyy HH:mm:ss");
+        Date date = new Date();
+        Date expireDate;
+        try {
+            expireDate = dateFormat.parse(expireDateString);
+        } catch (ParseException exc) {
+            return;
+        }
+
         /*
         Check if player is muted
          */
+        if (date.before(expireDate)) {
+            event.setCancelled(true);
+        }
 
         /*
-        Handle duration
+        Handle punishment duration input
          */
         if (UserManagerSystem.getDuration) {
             event.setCancelled(true);
